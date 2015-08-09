c05fe67883797e8999760dd7f33b636f912ccfac
brij-mishra <brij.mishra@outlook.com>
2015-08-10 01:45:51 +0530

    AngularJS-Day2
    Code sample for Day 2

---
+++ AngularJS-Part2/AngularJS-Part2/Scripts/Events/eventController.js
@@ -0,0 +1,8 @@
+﻿eventModule.controller("eventController", function ($scope) {
+    $scope.talks = [
+     { id: '1001', name: 'Real Time Web Applications with SignalR', speaker: 'Brij Bhushan Mishra', venue: 'Hall 1', duration: '45m' },
+     { id: '1002', name: 'Power of Node.js', speaker: 'Dhananjay Kumar', venue: 'Hall 2', duration: '45m' },
+     { id: '1003', name: 'Getting started with AngularJS', speaker: 'Brij Bhushan Mishra', venue: 'Hall 1', duration: '60m' },
+     { id: '1004', name: 'Microsoft Azure - Your cloud destination', speaker: 'Gaurav mantri', venue: 'Hall 1', duration: '45m' }
+    ];
+});
